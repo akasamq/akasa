c6e0c549105a536a47ecd7042563433932fce78e
Linfeng Qian <thewawar@gmail.com>
2022-12-16 14:11:05 +0800

    Add more tests for connect process

--- src/tests/protocol/connect.rs
+++ src/tests/protocol/connect.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 use mqtt::{
     control::variable_header::ConnectReturnCode,
     packet::{ConnackPacket, ConnectPacket, VariablePacket},
+    TopicName,
 };
 use tokio::{task::JoinHandle, time::sleep};
 use ConnectReturnCode::*;
@@ -29,7 +30,180 @@ async fn do_test(
 }
 
 #[tokio::test]
-async fn test_connect_simple() {
+async fn test_connect_malformed_packet() {
+    let (conn, mut control) = MockConn::new(3333, Config::default());
+    let join = control.start(conn);
+    control.write_data(b"abcdefxyzxyz123123".to_vec()).await;
+    sleep(Duration::from_millis(10)).await;
+    assert!(control.try_read_packet().is_err());
+    assert!(join.is_finished());
+    assert!(join.await.unwrap().is_err());
+}
+
+#[tokio::test]
+async fn test_connect_v310() {
+    // connect accepted
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let connect = ConnectPacket::with_level("MQIsdp", "client_anonymous", 0x03).unwrap();
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, ConnectionAccepted).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(!join.is_finished());
+    }
+    // connect identifier rejected: empty identifier
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let connect = ConnectPacket::with_level("MQIsdp", "", 0x03).unwrap();
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, IdentifierRejected).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(join.is_finished());
+    }
+    // connect identifier rejected: identifier too large
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let connect = ConnectPacket::with_level("MQIsdp", "a".repeat(24), 0x03).unwrap();
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, IdentifierRejected).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(join.is_finished());
+    }
+}
+
+#[tokio::test]
+async fn test_connect_v311() {
+    // connect accepted: identifier empty
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let connect = ConnectPacket::new("");
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, ConnectionAccepted).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(!join.is_finished());
+    }
+}
+
+#[tokio::test]
+async fn test_connect_invalid_protocol() {
+    // connect rejrected: invalid protocol name
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let connect = ConnectPacket::with_level("MQISDP", "aaa", 0x03).unwrap();
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket =
+            ConnackPacket::new(false, UnacceptableProtocolVersion).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(join.is_finished());
+    }
+    // connect rejrected: invalid protocol level
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let connect = ConnectPacket::with_level("MQTT", "aaa", 0x03).unwrap();
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket =
+            ConnackPacket::new(false, UnacceptableProtocolVersion).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(join.is_finished());
+    }
+}
+
+#[tokio::test]
+async fn test_connect_keepalive() {
+    // connect accepted: zero keep_alive
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let mut connect = ConnectPacket::new("client identifier");
+        connect.set_keep_alive(0);
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, ConnectionAccepted).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(!join.is_finished());
+    }
+
+    // connect accepted: non-zero keep_alive
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let mut connect = ConnectPacket::new("client identifier");
+        connect.set_keep_alive(22);
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, ConnectionAccepted).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(!join.is_finished());
+    }
+}
+
+#[tokio::test]
+async fn test_will() {
+    // connect accepted: with will
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let mut connect = ConnectPacket::new("client identifier");
+        connect.set_will(Some((TopicName::new("topic/1").unwrap(), vec![1, 2, 3, 4])));
+        connect.set_will_qos(1);
+        control.write_packet(connect.into()).await;
+        let packet = control.read_packet().await;
+        let expected_packet: VariablePacket = ConnackPacket::new(false, ConnectionAccepted).into();
+        assert_eq!(packet, expected_packet);
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(!join.is_finished());
+    }
+
+    // connect accepted: with invalid will topic (start with "$")
+    {
+        let (conn, mut control) = MockConn::new(3333, Config::default());
+        let join = control.start(conn);
+        let mut connect = ConnectPacket::new("client identifier");
+        connect.set_will(Some((
+            TopicName::new("$topic/1").unwrap(),
+            vec![1, 2, 3, 4],
+        )));
+        connect.set_will_qos(1);
+        control.write_packet(connect.into()).await;
+        assert!(control.try_read_packet().is_err());
+
+        sleep(Duration::from_millis(10)).await;
+        assert!(join.is_finished());
+        assert!(join.await.unwrap().is_err());
+    }
+}
+
+#[tokio::test]
+async fn test_connect_auth() {
     // allow anonymous
     {
         let config = Config::default();
@@ -78,15 +252,4 @@ async fn test_connect_simple() {
         let connack = ConnackPacket::new(false, ConnectionAccepted);
         do_test(config, connect, connack).await;
     }
-}
-
-#[tokio::test]
-async fn test_connect_malformed_packet() {
-    let (conn, mut control) = MockConn::new(3333, Config::default());
-    let join = control.start(conn);
-    control.write_data(b"abcdefxyzxyz123123".to_vec()).await;
-    sleep(Duration::from_millis(10)).await;
-    assert!(control.try_read_packet().is_err());
-    assert!(join.is_finished());
-    assert!(join.await.unwrap().is_err());
 }
